fc9a8cf514e4172ea3e3abdda6fca8bf475638d8
hansol1993 <145645339+hansol1993@users.noreply.github.com>
2023-11-13 19:14:02 +0900

    1113

--- team_project/src/main/webapp/js/main.js
+++ team_project/src/main/webapp/js/main.js
@@ -1,23 +1,5 @@
 
 //팝업창
-function getCookie( name ){
-	var nameOfCookie = name + "=";
-	var x = 0;
-	while ( x <= document.cookie.length )
-	{
-			var y = (x+nameOfCookie.length);
-			if ( document.cookie.substring( x, y ) == nameOfCookie ) {
-					if ( (endOfCookie=document.cookie.indexOf( ";", y )) == -1 )
-							endOfCookie = document.cookie.length;
-					return unescape( document.cookie.substring( y, endOfCookie ) );
-			}
-			x = document.cookie.indexOf( " ", x ) + 1;
-			if ( x == 0 )
-					break;
-	}
-	return "";
-}
-
 if ( getCookie( "Notice" ) !="done") {
 		noticeWindow  =  window.open('/dog/popup','notice','left=0, top=0, width=400,height=400');
 		noticeWindow.opener = self;
